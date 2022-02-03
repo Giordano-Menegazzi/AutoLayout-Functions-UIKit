276a639316b53fc70f4e398954e9228783716b2e
Giordano Menegazzi <71626443+Giordano-Menegazzi@users.noreply.github.com>
2022-02-03 22:54:04 +0100

    Update UIViewExtension.swift

--- UIViewExtension.swift
+++ UIViewExtension.swift
@@ -10,29 +10,15 @@ import UIKit
 
 // MARK: - UIView Anchoring Extension
 /// this extension handles all the custom UIView anchoring constraint functions "AutoLayout!!"
-extension UIView
-{
+extension UIView {
     // MARK: - Combined Anchoring Functions
     /// this function lets you set all the anchors / paddings and sizes of an UIViewObject all in one function
-    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero)
-    {
+    func anchor(top: NSLayoutYAxisAnchor? = nil, leading: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, trailing: NSLayoutXAxisAnchor? = nil, padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
         translatesAutoresizingMaskIntoConstraints = false
-        
-        if let top = top {
-            topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
-        }
-        
-        if let leading = leading {
-            leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
-        }
-        
-        if let bottom = bottom {
-            bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
-        }
-        
-        if let trailing = trailing {
-            trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
-        }
+        if let top = top { topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true }
+        if let leading = leading {  leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true }
+        if let bottom = bottom { bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true }
+        if let trailing = trailing { trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true }
         
         if size.width != 0 {
             widthAnchor.constraint(equalToConstant: size.width).isActive = true
@@ -74,8 +60,7 @@ extension UIView
     
     // MARK: - Flexible Bottom Anchor Function
     /// this function anchors the bottom anchor with an greater than or eaqual to padding
-    func anchorBottomWithGreaterThanOrEqualToPadding(anchorTo: NSLayoutYAxisAnchor, padding: CGFloat)
-    {
+    func anchorBottomWithGreaterThanOrEqualToPadding(anchorTo: NSLayoutYAxisAnchor, padding: CGFloat) {
         translatesAutoresizingMaskIntoConstraints = false
         let bottom = bottomAnchor.constraint(equalTo: anchorTo, constant: -padding)
         bottom.priority = UILayoutPriority(rawValue: 250)
@@ -89,42 +74,34 @@ extension UIView
     
     // MARK: - ButtonStackView Anchoring Functions
     /// this function puts the 2 buttons in a stackview and anchors it
-    func anchorButtonsWithStackViewTop(inView: UIView, anchorTopTo: NSLayoutYAxisAnchor, padding: CGFloat, leftButton: NormalButton, rightButton: NormalButton)
-    {
+    func anchorButtonsWithStackViewTop(inView: UIView, anchorTopTo: NSLayoutYAxisAnchor, padding: CGFloat, leftButton: NormalButton, rightButton: NormalButton) {
         let btnSpacing: CGFloat = 20
         let ButtonStackView = customUIStackView(arrangedSubviews: [leftButton, rightButton], distribution: .fillEqually, axis: .horizontal, spacing: btnSpacing)
-        
         addSubview(ButtonStackView)
         ButtonStackView.anchor(top: anchorTopTo, leading: inView.leadingAnchor, bottom: nil, trailing: inView.trailingAnchor, padding: .init(top: padding, left: flexibleSizes.controlsSidePadding, bottom: 0, right: flexibleSizes.controlsSidePadding))
     }
     
     /// this function puts the 2 buttons in a stackview and anchors it with the top and an greater than or equal bottom anchor
-    func anchorButtonsWithStackViewTopAndBottomGreaterThanOrEqual(inView: UIView, anchorTopTo: NSLayoutYAxisAnchor, anchorBottomTo: NSLayoutYAxisAnchor , topPadding: CGFloat, bottomPadding: CGFloat, leftButton: NormalButton, rightButton: NormalButton)
-    {
+    func anchorButtonsWithStackViewTopAndBottomGreaterThanOrEqual(inView: UIView, anchorTopTo: NSLayoutYAxisAnchor, anchorBottomTo: NSLayoutYAxisAnchor , topPadding: CGFloat, bottomPadding: CGFloat, leftButton: NormalButton, rightButton: NormalButton) {
         let btnSpacing: CGFloat = 20
         let ButtonStackView = customUIStackView(arrangedSubviews: [leftButton, rightButton], distribution: .fillEqually, axis: .horizontal, spacing: btnSpacing)
-        
         addSubview(ButtonStackView)
         ButtonStackView.anchor(top: anchorTopTo, leading: inView.leadingAnchor, bottom: nil, trailing: inView.trailingAnchor, padding: .init(top: topPadding, left: flexibleSizes.controlsSidePadding, bottom: 0, right: flexibleSizes.controlsSidePadding))
         ButtonStackView.anchorBottomWithGreaterThanOrEqualToPadding(anchorTo: anchorBottomTo, padding: bottomPadding)
     }
     
     /// this function puts the 2 buttons in a stackview and anchors it in a Popup
-    func anchorButtonsWithStackViewInPopupTop(inView: UIView, anchorTopTo: NSLayoutYAxisAnchor, padding: CGFloat, leftButton: NormalButton, rightButton: NormalButton)
-    {
+    func anchorButtonsWithStackViewInPopupTop(inView: UIView, anchorTopTo: NSLayoutYAxisAnchor, padding: CGFloat, leftButton: NormalButton, rightButton: NormalButton) {
         let btnSpacing: CGFloat = 20
         let ButtonStackView = customUIStackView(arrangedSubviews: [leftButton, rightButton], distribution: .fillEqually, axis: .horizontal, spacing: btnSpacing)
-        
         addSubview(ButtonStackView)
         ButtonStackView.anchor(top: anchorTopTo, leading: inView.leadingAnchor, bottom: nil, trailing: inView.trailingAnchor, padding: .init(top: padding, left: flexibleSizes.controlsSidePaddingPopup, bottom: 0, right: flexibleSizes.controlsSidePaddingPopup))
     }
     
     /// this function puts the 2 buttons in a stackview and anchors it with the top and an greater than or equal bottom anchor in a Popup
-    func anchorButtonsWithStackViewInPopupTopAndBottomGreaterThanOrEqual(inView: UIView, anchorTopTo: NSLayoutYAxisAnchor, anchorBottomTo: NSLayoutYAxisAnchor , topPadding: CGFloat, bottomPadding: CGFloat, leftButton: NormalButton, rightButton: NormalButton)
-    {
+    func anchorButtonsWithStackViewInPopupTopAndBottomGreaterThanOrEqual(inView: UIView, anchorTopTo: NSLayoutYAxisAnchor, anchorBottomTo: NSLayoutYAxisAnchor , topPadding: CGFloat, bottomPadding: CGFloat, leftButton: NormalButton, rightButton: NormalButton) {
         let btnSpacing: CGFloat = 20
         let ButtonStackView = customUIStackView(arrangedSubviews: [leftButton, rightButton], distribution: .fillEqually, axis: .horizontal, spacing: btnSpacing)
-        
         addSubview(ButtonStackView)
         ButtonStackView.anchor(top: anchorTopTo, leading: inView.leadingAnchor, bottom: nil, trailing: inView.trailingAnchor, padding: .init(top: topPadding, left: flexibleSizes.controlsSidePaddingPopup, bottom: 0, right: flexibleSizes.controlsSidePaddingPopup))
         ButtonStackView.anchorBottomWithGreaterThanOrEqualToPadding(anchorTo: anchorBottomTo, padding: bottomPadding)
